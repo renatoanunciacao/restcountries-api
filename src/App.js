2725f1421cb003d74399afdeea8e3f1e7832feb3
Renato Anunciação <wagnerpunk002@gmail.com>
2019-12-26 09:37:16 -0300

    envio do primeiro commit da aplicação

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './pages/Home/Home';
+import Detail from './pages/Detail/Detail';
+import Favs from './pages/Favs/Favs';
+
+import GlobalStyle from './assets/styles/Global';
+const App = props => (
+  <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Home />} />
+        <Route path="/country/:numericcode" render={localProps => <Detail {...localProps} />} />
+        <Route path="/favs" render={() => <Favs />} />
+      </Switch>
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
+
 
 export default App;
---
+++ src/pages/Favs/Favs.jsx
@@ -0,0 +1,7 @@
+
+
+import React from 'react';
+
+const Favs = () => <div>Favs</div>
+
+export default Favs;
